f9b5031d58a92cf2d71d014d57112b3c93e39a2a
Andres Gasca <andresg1006@gmail.com>
2023-10-19 16:18:58 -0600

    Se agrega modulo de authenticacion

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,26 +1,49 @@
 <template>
-    <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'entry', params: { id: 10 } })">
+    <div class="entry-container mb-3 pointer p-2" @click="$router.push({ name: 'entry', params: { id: entry.id } })">
 
         <!-- Titulo -->
         <div class="entry-title d-flex">
-            <span class="text-success fs-5 fw-bold">12</span>
-            <span class="mx-1 fs-5">Septiembre</span>
-            <span class="mx-2 fw-light">2023, martes</span>
+            <span class="text-success fs-5 fw-bold">{{ day }}</span>
+            <span class="mx-1 fs-5">{{ month }}</span>
+            <span class="mx-2 fw-light">{{ yearDay }}</span>
         </div>
 
         <div class="entry-description">
-            Ullamco culpa voluptate est ex cupidatat culpa elit labore quis elit tempor. Laborum nulla qui qui duis eiusmod
-            ullamco cillum est ipsum eiusmod aliqua ut. Ullamco et eu anim tempor eu nisi sunt culpa sit duis esse
-            consectetur ipsum. Aute esse anim occaecat ex laborum adipisicing reprehenderit consequat deserunt. Proident
-            ullamco pariatur non nostrud cupidatat irure officia culpa aliquip quis et fugiat. Fugiat est ullamco laboris
-            aliqua Lorem Lorem veniam exercitation est dolore incididunt.
+            {{ shortText }}
         </div>
     </div>
 </template>
 
 <script>
-export default {
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
 
+export default {
+    props: {
+        entry: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        shortText() {
+            return (this.entry.text.length > 130)
+                ? this.entry.text.substring(0, 130) + '...'
+                : this.entry.text
+        },
+        day() {
+            const date = new Date(this.entry.date)
+            return date.getDate()
+        },
+        month() {
+            const date = new Date(this.entry.date)
+            return months[date.getMonth()]
+        },
+        yearDay() {
+            const date = new Date(this.entry.date)
+            return `${date.getFullYear()}, ${days[date.getDay()]}`
+        },
+    }
 }
 </script>
 
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -3,19 +3,19 @@ export default () => ({
     isLoadin: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'Ullamco reprehenderit fugiat anim dolor. Tempor Lorem veniam consequat laboris non cupidatat cillum magna. Laborum ipsum magna officia nostrud quis reprehenderit sit deserunt.',
             picture: null,
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Anim dolore cupidatat proident dolor elit officia pariatur in magna consectetur officia. Magna tempor amet dolore cupidatat consectetur amet consectetur culpa consectetur. Culpa in sint ipsum ex duis laborum esse et aliqua eu. Consectetur eu commodo consequat deserunt quis quis elit fugiat minim. Incididunt eu nisi deserunt ad. Reprehenderit velit anim sit ipsum duis elit magna id qui Lorem officia eu minim.',
             picture: null,
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
             text: 'Elit et voluptate cupidatat quis laboris nulla culpa cupidatat sint fugiat duis. Aute nisi ea labore proident tempor nulla. Qui incididunt voluptate voluptate ea enim sit incididunt laboris ipsum ullamco officia. Elit ipsum velit ad voluptate esse pariatur minim cillum labore. Tempor dolor exercitation officia laboris dolore id labore eu reprehenderit nostrud minim velit.',
             picture: null,
